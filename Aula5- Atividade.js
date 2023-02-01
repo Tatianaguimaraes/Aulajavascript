f543cd6fde8b4c05852d6d67b1280a9f2f4b02d3
Tatiana Guimarães <tatianaguimaraes10@gmail.com>
2023-01-31 22:22:06 -0300

    aula5

---
+++ Aula5- Atividade.js
@@ -0,0 +1,26 @@
+//Encontre de 3 a 5 expressões que o resultado é verdadeiro ou true 
+//Encontre de 3 a 5 expressões que o resultado é falso ou false
+
+//Operadores Relacionais 
+// >, <, <=, >=, ==, ===, !=
+
+let numero1 = 19;
+let numero2 = 11;
+let numero3 = 19;
+
+//Verdadeiro (true)
+console.log(19>11);
+console.log(19>=11);
+console.log(11<=19);
+console.log(19==19);
+console.log(11!=19);
+console.log(numero2===numero2);
+
+
+//Falso (False)
+console.log(11>19);
+console.log(11>=19);
+console.log(19<=11);
+console.log(19==11);
+console.log(19!=19);
+console.log(numero2===numero3);
